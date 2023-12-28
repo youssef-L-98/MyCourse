973c0d0802472abad81fdceeacf5d6b22eb5c3b4
Youssef 98 <laamariyoussef43@gmail.com>
2023-12-28 13:04:00 +0100

    aggiunta dei primi model

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyCourse.Models.Services.Application;
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Controllers
 {
@@ -10,7 +12,10 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //posso anche scrivere var courseService = new CourseService();
+            CourseService courseService = new CourseService();
+            List <CourseViewModel> courses = courseService.GetServices();//vogliamo che l'oggetto courses tramita l'oggetto courseService vada a prendere tutti i corsi disposti in una lista
+            return View(courses); //passiamo l'oggetto contenente l'elenco di tutti i corsi alla view che se ne deve occupare
         }
         
         public IActionResult Detail(string id)
---
+++ src/MyCourse/Models/Services/Application/CourseService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyCourse.Models.ViewModels;
+
+namespace MyCourse.Models.Services.Application
+{
+    public class CourseService
+    {
+        public List<CourseViewModel> GetServices()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
