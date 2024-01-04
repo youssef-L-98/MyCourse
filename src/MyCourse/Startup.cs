8debc70f2d668a85e4f4a703d25520f65ed50d01
Youssef 98 <laamariyoussef43@gmail.com>
2024-01-04 19:56:39 +0100

    struttura della Dependecy Injection

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using MyCourse.Models.Services.Application;
 
 namespace MyCourse
 {
@@ -18,6 +19,7 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+            services.AddTransient<ICourseService, CourseService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
